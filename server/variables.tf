deaa361b87e99835ab1657a4929493f77eae4d1f
Sean Sandbox <sean@sean123456.com>
2017-08-09 13:17:52 +0100

    remove need for db_instance_size, as it should be more generic, and be any instance size

--- server/variables.tf
+++ server/variables.tf
@@ -1,7 +1,6 @@
 variable "region" {	default = "us-east-1" }
 variable "instance_type" { default = "t2.micro" }
 variable "key_name" { description = "The EC2 key pair name" }
-variable "db_instance_size" { description = "EC2 instance size for DB instance types" }
 variable "vpc_id" { description = "VPCs are pre-provisioned outside of Terraform's control. Provide the VPC ID to use." }
 variable "cloud_account_name" { description = "AWS Cloud Account name" }
 variable "environment_number_range" { description = "Environment range start number" }
