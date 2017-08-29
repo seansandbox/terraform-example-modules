ef12c056217b67ed2c57cf760f73b18f22da2b79
Sean Sandbox <sean@sean123456.com>
2017-08-29 14:57:52 +0100

    remote-exec changes

--- server/main.tf
+++ server/main.tf
@@ -70,7 +70,7 @@ resource "aws_instance" "default" {
     provisioner "remote-exec" {
       inline = [
         "mkdir C:\\remotedata",
-        "echo \"${var.instance_remote_data}\" > C:\\remotedata\\remotedata.txt"
+        "echo ${var.instance_remote_data} > C:\\remotedata\\remotedata.txt"
       ]
 
       connection {
